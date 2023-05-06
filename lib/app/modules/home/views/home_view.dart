97a4b135634cb0dbd63291f7514efa8c012e7b11
M Haris Wicaksono <hariswicaksono250999@gmail.com>
2023-05-06 15:42:36 +0700

    tf

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,16 +10,87 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Username",
+                  style: GoogleFonts.inter(
+                      textStyle:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.w500)),
+                ),
+                Text(
+                  "Welcome to YukKuy.",
+                  style: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                          fontSize: 15,
+                          color: Color.fromRGBO(143, 149, 158, 1))),
+                ),
+                TextField(),
+                SizedBox(
+                  height: 40,
+                  child: ListView.builder(
+                    physics: ClampingScrollPhysics(),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 15,
+                    itemBuilder: (BuildContext context, int index) => Chip(
+                      label: Text(
+                        'Surabaya',
+                        style: GoogleFonts.inter(
+                            textStyle: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                        )),
+                      ),
+                      backgroundColor: Color.fromRGBO(245, 246, 250, 1),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Text('Home'), label: ""),
+        BottomNavigationBarItem(icon: Text('Transaction'), label: ""),
+        BottomNavigationBarItem(icon: Text('Favorite'), label: ""),
+        BottomNavigationBarItem(icon: Text('Profile'), label: ""),
+      ]),
+      // bottomNavigationBar: Container(
+      //   color: Colors.amber,
+      //   height: 64,
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     children: [
+      //       Text(
+      //         'Home',
+      //         style: GoogleFonts.inter(
+      //             textStyle: TextStyle(color: Color.fromRGBO(25, 164, 99, 1))),
+      //       ),
+      //       Text(
+      //         'Transaction',
+      //         style: GoogleFonts.inter(
+      //             textStyle: TextStyle(color: Color.fromRGBO(25, 164, 99, 1))),
+      //       ),
+      //       Text(
+      //         'Favorite',
+      //         style: GoogleFonts.inter(
+      //             textStyle: TextStyle(color: Color.fromRGBO(25, 164, 99, 1))),
+      //       ),
+      //       Text(
+      //         'Profile',
+      //         style: GoogleFonts.inter(
+      //             textStyle: TextStyle(color: Color.fromRGBO(25, 164, 99, 1))),
+      //       )
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,49 @@ void main() {
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            errorMaxLines: 1,
+            // iconColor: CustomColor.disable,
+            filled: true,
+            fillColor: Color.fromRGBO(245, 246, 250, 1),
+            // hintStyle: CustomFonts.poppinsRegular12.copyWith(
+            //   color: CustomColor.disable,
+            // ),
+            // errorStyle: CustomFonts.poppinsRegular12.copyWith(
+            //   color: CustomColor.red,
+            // ),
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 20,
+            ),
+            // focusedBorder: OutlineInputBorder(
+            //   borderRadius: BorderRadius.circular(5),
+            //   borderSide: BorderSide(
+            //     width: 1,
+            //     // color: CustomColor.disable,
+            //   ),
+            // ),
+            border: InputBorder.none,
+            focusedBorder: InputBorder.none
+            // enabledBorder: OutlineInputBorder(
+            //   borderRadius: BorderRadius.circular(5),
+            //   borderSide: BorderSide(
+            //     width: 1,
+            //     // color: CustomColor.disable,
+            //   ),
+            // ),
+            // errorBorder: OutlineInputBorder(
+            //   borderRadius: BorderRadius.circular(5),
+            //   borderSide: BorderSide(
+            //     width: 1,
+            //     // color: CustomColor.red,
+            //   ),
+            // ),
+            ),
+      ),
     ),
   );
 }
